865b466651c7e47af2e6a4d74ede76b6e8d81075
Virgiliu Tirdea <virgiliustsirdea@yandex.ru>
2018-11-24 01:57:02 +0100

    homework

--- my_module.py
+++ my_module.py
@@ -1,4 +1,11 @@
 def divide_two_numbers(nr1,nr2):
     return nr1/nr2
+
 def occurence(str1,str2):
     return str2.count(str1)
+
+def extract_letter(str1,str2):
+    if str2 in str1:
+        return str1.replace(str2,'')
+    else:
+        return str1
---
+++ my_script1.py
@@ -0,0 +1,7 @@
+import sys
+from my_module import extract_letter
+
+str1 = sys.argv[1]
+str2 = sys.argv[2]
+
+print(extract_letter(str1,str2))
